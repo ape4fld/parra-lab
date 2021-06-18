0e30650da21978b895f95e4ee25e5c4c7a91b609
Frida Lona Durazo <32912529+ape4fld@users.noreply.github.com>
2021-06-18 13:57:33 -0400

    Update post-COJO.sh

--- GCTA/COJO/post-COJO.sh
+++ GCTA/COJO/post-COJO.sh
@@ -1,7 +1,7 @@
 # this script is very quick since the files are just a few lines each max. If using Compute Canada, can run it using --salloc or in the main node.
 
 # get a list of the chromosomes for which there is an output:
-ls /your-path-to-COJO-output/pheno-chr*.jma.cojo > list-COJO-chrs.txt
+ls /your-path-to-COJO-output/chr*-GCTA-COJO.jma.cojo > list-COJO-chrs.txt
 
 # extract the header:
 file1=$(sed -n 1p list-COJO-chrs.txt)
