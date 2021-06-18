aac16352ea9583f1a3264c7e358d7ac0100e5ab1
Frida Lona Durazo <32912529+ape4fld@users.noreply.github.com>
2021-06-18 15:45:24 -0400

    Update pre-meta-step2.sh

--- Metasoft/pre-meta-step2.sh
+++ Metasoft/pre-meta-step2.sh
@@ -1,6 +1,24 @@
+#!/bin/bash
+#SBATCH --account=def-eparra
+#SBATCH --time=02:00:00
+#SBATCH --array=1-22
+#SBATCH --job-name=pre-meta-step2
+#SBATCH --error=slurm-%x.err
+#SBATCH --output=slurm-%x.out
+#SBATCH --mail-user=youremail@address.com
+#SBATCH --mail-type=ALL
+#SBATCH --mem-per-cpu=4G
+#SBATCH --ntasks=3
 
 module load nixpkgs/16.09 gcc/5.4.0
 module load python/3.8.0
 module load r/3.3.3
 
-python plink2metasoft2.py chr${SLURM_ARRAY_TASK_ID} study1-chr${SLURM_ARRAY_TASK_ID}.out study2-chr${SLURM_ARRAY_TASK_ID}.out study3-chr${SLURM_ARRAY_TASK_ID}.out
+# This script uses the python script that comes along when you download Metasoft. The first line is calling the python script, the second line is the output name,
+# followed by lines with the summary statistics for each study.
+
+python plink2metasoft2.py \
+  /your-path-to-save-output/chr${SLURM_ARRAY_TASK_ID} \
+  /your-path-with-summary-stats/study1-chr${SLURM_ARRAY_TASK_ID}.out \
+  /your-path-with-summary-stats/study2-chr${SLURM_ARRAY_TASK_ID}.out \
+  /your-path-with-summary-stats/study3-chr${SLURM_ARRAY_TASK_ID}.out
