274c4e446156bf5d91bf30dfb36914706c2f90cf
Frida Lona Durazo <32912529+ape4fld@users.noreply.github.com>
2021-06-21 14:09:30 -0400

    Update ggmanhattan-function.R

--- plots/ggmanhattan-function.R
+++ plots/ggmanhattan-function.R
@@ -45,7 +45,7 @@ gg.manhattan <- function(path, df, threshold = 5e-8, colours = c("#E69F00", "#56
     ggplot(df.tmp, aes(x=BPcum, y=-log10(P))) +
     # Show all points
     geom_point(aes(color=as.factor(CHR)), alpha=0.8, size=2) +
-    scale_color_manual(values = rep(col, 22 )) +
+    scale_color_manual(values = rep(colours, 22 )) +
     
     # custom X axis:
     scale_x_continuous( label = axisdf$CHR, breaks= axisdf$center ) +
@@ -62,7 +62,7 @@ gg.manhattan <- function(path, df, threshold = 5e-8, colours = c("#E69F00", "#56
     geom_point(data=subset(df.tmp, is_highlight=="yes"), color="red", size=3) +
     
     # Add label using ggrepel to avoid overlapping
-    geom_label_repel(data=df.tmp[df.tmp$is_annotate=="yes",], aes(label=as.factor(SNP), alpha=0.7), size=5, force=1.3) +
+    # geom_label_repel(data=df.tmp[df.tmp$is_annotate=="yes",], aes(label=as.factor(SNP), alpha=0.7), size=5, force=1.3) +
     
     # Custom the theme:
     theme_bw(base_size = 22) +
