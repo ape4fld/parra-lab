2a5ff5da5d5b852538320fa891b6eaf0ac5d8243
Frida Lona Durazo <32912529+ape4fld@users.noreply.github.com>
2021-06-21 11:20:30 -0400

    Update compute-qqplots.R

--- plots/compute-qqplots.R
+++ plots/compute-qqplots.R
@@ -8,7 +8,12 @@ library(here)
 # Import the function:
 source(here("path-where-the-function-is-saved/", "qqplot-function.R"))
 
-# Run the function to generate and save a qqplot:
+# Run the function to generate and save a qqplot
+#### path_to_folder: path where the data is saved.
+#### file_to_read1: text file with one column (no header) of p-values.
+#### file_name: suffix of choice for the plot's name.
+#### lambda_pos: position of the lambda value on the Y-axis in the qqplot.
+
 post_gwas_plots(path_to_folder = "path-to-directory", 
                 file_to_read1 = "meta-pheno-pval.out", 
                 file_name = "meta-pheno", lambda_pos = 10)
