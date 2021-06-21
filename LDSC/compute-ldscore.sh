d8b9186b6c6edf1d6538923be9f5a44125017049
Frida Lona Durazo <32912529+ape4fld@users.noreply.github.com>
2021-06-21 16:29:19 -0400

    Create compute-ldscore.sh

---
+++ LDSC/compute-ldscore.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --partition=generalq
+#SBATCH --job-name=ldsc-compute
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=youremail@address.com
+#SBATCH --cpus-per-task=3
+#SBATCH --mem-per-cpu=2gb
+#SBATCH --time=1:0:0
+#SBATCH --output=ldsc-compute.out
+#SBATCH --error=ldsc-compute.err
+
+module load python/2.7.14
+module load ldsc/1.0.1
+
+ldsc.py \
+        --h2 ldsc-summary-stats.sumstats.gz \
+        --ref-ld-chr eur_w_ld_chr/ \
+        --w-ld-chr eur_w_ld_chr/ \
+        --out /your-path-to-save-output/LDScore-regression-gwas
