a99b8560f54763a86e6327ab81703f22c4d36d78
Frida Lona Durazo <32912529+ape4fld@users.noreply.github.com>
2021-06-21 16:08:21 -0400

    Update munge-sumstats.sh

--- FUSION/munge-sumstats.sh
+++ FUSION/munge-sumstats.sh
@@ -17,7 +17,7 @@ module load ldsc/1.0.1
 # The munge_sumstats.py script used here is available at the LDSC github page: https://github.com/bulik/ldsc/blob/master/munge_sumstats.py
 # This script is included in the LDSC package available in Calculon.
 # More about munge sumstats: https://github.com/bulik/ldsc/wiki/Summary-Statistics-File-Format
-# Note 2: Some common errors arise due to the order of A1 and A2 in the sumstats compared to the LD reference file!
+# Note 2: Some common errors arise due to misalignment of A1 and A2 in the sumstats compared to the LD reference file!
 
 python munge_sumstats.py \
 	--sumstats gwas-summary-stats.out \
