b9579a8557b3cf671b9bf0a99671b05d2bba4fdd
Frida Lona Durazo <32912529+ape4fld@users.noreply.github.com>
2021-06-21 17:50:40 -0400

    Update get-bgen-subset-files.sh

--- FINEMAP/get-bgen-subset-files.sh
+++ FINEMAP/get-bgen-subset-files.sh
@@ -17,16 +17,19 @@
 # Note 2: the number of batch arrays (see header of script above), corresponds to the number of regions or subset to finemap.
 
 # extract the chromosome number for each region:
+
 chr=$(sed -n ${SLURM_ARRAY_TASK_ID}p HC-M${model}-posminmax.txt | awk '{print $1}')
 
 # Step 1: convert from VCF to BGEN, which retains the imputation information:
+
 qctool_v2.0.7 \
         -g /your-path-to-the-imputed-genotype-data/${chr}.vcf.gz -vcf-genotype-field GP \
-        -incl-rsids /path-to-lists-of-rsids-from-subsets/rsids-subset${SLURM_ARRAY_TASK_ID}.txt \
+        -incl-rsids /path-to-lists-of-rsids-from-subsets/rsids-sumstats-for-finemap-subset${SLURM_ARRAY_TASK_ID}.out \
         -og /path-to-output-bgen-files/finemap-subset${SLURM_ARRAY_TASK_ID}.bgen \
         -os /path-to-output-bgen-files/finemap-subset${SLURM_ARRAY_TASK_ID}.sample
 
 # Step 2: generate a file with the SNP information in the new files, to check that there is a full overlap between the SNPs in the BGEN subset files and the summary statistics subset files.
-# this is important because LDstore requires a full overlap of the SNPs, otherwise it will generate errors.
+# Note 3: this is important because LDstore requires a full overlap of the SNPs, otherwise it will generate errors.
+
 qctool_v2.0.7 \
         -g /path-of-bgen-output-files/finemap-subset${SLURM_ARRAY_TASK_ID}.bgen -snp-stats -osnp /path-of-bgen-output-files/finemap-subset${SLURM_ARRAY_TASK_ID}.txt
