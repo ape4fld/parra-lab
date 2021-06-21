70c5dccb586823f25a3113331bb566055a71291d
Frida Lona Durazo <32912529+ape4fld@users.noreply.github.com>
2021-06-21 18:38:06 -0400

    Update run-FINEMAP.sh

--- FINEMAP/run-FINEMAP.sh
+++ FINEMAP/run-FINEMAP.sh
@@ -16,7 +16,8 @@ module load finemap/1.4
 
 # you can change the --n-causal-snps for any value between 1 and 20.
 # similar to LDstore, you can specify --dataset the subset you want to run.
+# the --force-n-samples is used when the sample size in the BGEN file is different from the GWAS sample size (indicated in the masterfiles for LDstore and FINEMAP).
 
-finemap --sss --in-files masterfile-FINEMAP-example.txt --log --n-causal-snps 10 --std-effects --n-threads 5
+finemap --sss --in-files masterfile-FINEMAP-example.txt --log --n-causal-snps 10 --std-effects --n-threads 5 --force-n--samples
 
 # there are various outputs for each subset, check out the FINEMAP website for details and how to interpret them.
