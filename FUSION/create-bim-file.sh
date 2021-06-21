881ed2b44b8791714dac9fbe6c6e1a0ef8d0ecf8
Frida Lona Durazo <32912529+ape4fld@users.noreply.github.com>
2021-06-21 15:08:38 -0400

    Update create-bim-file.sh

--- FUSION/create-bim-file.sh
+++ FUSION/create-bim-file.sh
@@ -1,1 +1,7 @@
-bim-file-all-chrs.txt
+# This is a quick step that can be run with --salloc on Compute Canada.
+# The script merges the bim files (after filtering) for all chromosomes generated in the previous step, to use in the next step (munge sumstats).
+
+for i in 1:22
+do
+  cat /path-with-LDREF-files/LDref-filtered-chr${i} >> bim-file-all-chrs.txt
+done
